77bd223ca5f400f14de1c2e3b986edc533fdde49
deviceai <device.ai@gmail.com>
2020-04-01 22:48:56 +0300

    get access to private fields

--- Reflection/reflection-demo/src/main/java/Main.java
+++ Reflection/reflection-demo/src/main/java/Main.java
@@ -7,7 +7,25 @@ public class Main {
         Class<SomeClass> someClassAsClass = (Class<SomeClass>) someObject.getClass();
 
         Field someField = someClassAsClass.getField("someField");
-        System.out.println(someField.getType());
+        //System.out.println(someField.getType());
+
+        Field[] fields = someClassAsClass.getFields();
+        for(Field field : fields) {
+            System.out.println(field.getType() + " " + field.getName());
+        }
+
+        System.out.println(someObject.someField);
+        someField.set(someObject, 777);
+        System.out.println(someObject.someField);
+
+        //get access to private fields
+        Field pField = someClassAsClass.getDeclaredField("somePrivateString");
+        System.out.println(pField.getType() + " " + pField.getName());
+        pField.setAccessible(true);
+        pField.set(someObject, "John");
+        System.out.println(someObject.getSomePrivateString());
+
+
 
     }
 }
--- Reflection/reflection-demo/src/main/java/SomeClass.java
+++ Reflection/reflection-demo/src/main/java/SomeClass.java
@@ -1,3 +1,10 @@
 public class SomeClass {
     public int someField;
+    public String someString;
+    private String somePrivateString;
+
+    public String getSomePrivateString() {
+        return somePrivateString;
+    }
+
 }
