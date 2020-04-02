91abbe4f2cfe43707d2d86c71a73fe67d8639a71
deviceai <device.ai@gmail.com>
2020-04-02 15:31:34 +0300

    add hibernate example Employee

---
+++ Hibernate/hibernate-employee/src/main/java/app/ManageEmployee.java
@@ -0,0 +1,125 @@
+package app;
+
+import model.Employee;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+import java.util.Iterator;
+import java.util.List;
+
+public class ManageEmployee {
+    private static SessionFactory factory;
+
+    public static void main(String[] args) {
+
+        factory = new Configuration().configure().buildSessionFactory();
+        ManageEmployee ME = new ManageEmployee();
+
+        Integer emp001 = ME.addEmployee("John", "Smith", 2500);
+        Integer emp002 = ME.addEmployee("Tiim", "Kukk", 10000);
+        Integer emp003 = ME.addEmployee("Alex", "Paar", 4000);
+        Integer emp004 = ME.addEmployee("Test", "Test", 9999);
+        Integer emp005 = ME.addEmployee("Bob", "Marley", 1000);
+
+        /* List down all the employees */
+        ME.listEmployees();
+
+        /* Update employee's records */
+        ME.updateEmployee(emp001, 5000);
+
+        /* Delete an employee from the database */
+        ME.deleteEmployee(emp004);
+
+        /* List down new list of the employees */
+        ME.listEmployees();
+
+    }
+
+    /* Method to  READ all the employees */
+    private void listEmployees() {
+        Session session = factory.openSession();
+        Transaction tx = null;
+
+        try {
+            tx = session.beginTransaction();
+            List employees = session.createQuery("FROM Employee").list();
+            for (Iterator iterator = employees.iterator(); iterator.hasNext(); ) {
+                Employee employee = (Employee) iterator.next();
+                System.out.print("First Name: " + employee.getFirstName());
+                System.out.print("  Last Name: " + employee.getLastName());
+                System.out.println("  Salary: " + employee.getSalary());
+            }
+            tx.commit();
+        } catch (HibernateException e) {
+            if (tx != null) tx.rollback();
+            e.printStackTrace();
+        } finally {
+            session.close();
+        }
+
+    }
+
+    /* Method to CREATE an employee in the database */
+    public Integer addEmployee(String fname, String lname, int salary) {
+        Session session = factory.openSession();
+        Transaction tx = null;
+        Integer employeeID = null;
+
+        try {
+            tx = session.beginTransaction();
+            Employee employee = new Employee(fname, lname, salary);
+            employeeID = (Integer) session.save(employee);
+            tx.commit();
+        } catch (HibernateException e) {
+            if (tx != null) tx.rollback();
+            e.printStackTrace();
+        } finally {
+            session.close();
+        }
+        return employeeID;
+    }
+
+    public void deleteEmployee(Integer EmployeeID){
+        Session session = factory.openSession();
+        Transaction tx = null;
+
+        try {
+            tx = session.beginTransaction();
+            Employee employee = (Employee)session.get(Employee.class, EmployeeID);
+            session.delete(employee);
+            tx.commit();
+        } catch (HibernateException e) {
+            if (tx!=null) tx.rollback();
+            e.printStackTrace();
+        } finally {
+            session.close();
+        }
+    }
+
+    /* Method to UPDATE salary for an employee */
+    public void updateEmployee(Integer EmployeeID, int salary ){
+        Session session = factory.openSession();
+        Transaction tx = null;
+
+        try {
+            tx = session.beginTransaction();
+            Employee employee = (Employee)session.get(Employee.class, EmployeeID);
+            employee.setSalary( salary );
+            session.update(employee);
+            tx.commit();
+        } catch (HibernateException e) {
+            if (tx!=null) tx.rollback();
+            e.printStackTrace();
+        } finally {
+            session.close();
+        }
+    }
+
+
+
+}
+
+
