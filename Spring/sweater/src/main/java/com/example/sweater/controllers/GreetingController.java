3ec7c4373994a1ffcb55c315f946bab9eedcad40
deviceai <device.ai@gmail.com>
2020-04-21 17:49:13 +0300

    sweater app - first commit + mustache

--- Spring/sweater/src/main/java/com/example/sweater/controllers/GreetingController.java
+++ Spring/sweater/src/main/java/com/example/sweater/controllers/GreetingController.java
@@ -5,6 +5,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.Map;
+
 @Controller
 public class GreetingController {
 
@@ -12,8 +14,8 @@ public class GreetingController {
     public String greeting(
             @RequestParam(name="name", required = false, defaultValue = "Unknown person")
                     String name,
-           Model model){
-        model.addAttribute("name", name);
+                    Map<String, Object> model){
+        model.put("name", name);
         return "greeting";
     }
 
