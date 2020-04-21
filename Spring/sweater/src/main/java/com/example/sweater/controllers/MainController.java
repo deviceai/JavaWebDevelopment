612596f8ccd60eb1ae4fbf60105fcbf566a4d24c
deviceai <device.ai@gmail.com>
2020-04-21 18:48:25 +0300

    sweater messages adding and filtering

--- Spring/sweater/src/main/java/com/example/sweater/controllers/MainController.java
+++ Spring/sweater/src/main/java/com/example/sweater/controllers/MainController.java
@@ -1,15 +1,62 @@
 package com.example.sweater.controllers;
 
+import com.example.sweater.domain.Message;
+import com.example.sweater.repo.MessageRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+import java.util.Map;
 
 @Controller
 public class MainController {
 
+    @Autowired
+    private MessageRepo messageRepo;
+
     @GetMapping("/")
-    public String home(Model model) {
-        //model.addAttribute("title", "This is home page");
+    public String home(Map<String, Object> model) {
+        Iterable<Message> messages = messageRepo.findAll();
+
+        model.put("messages", messages);
         return "home";
     }
+
+    @PostMapping("add")
+    public String addMessage(
+            @RequestParam String text,
+            @RequestParam String tag,
+            Map<String, Object> model
+    ){
+        //Create new message
+        Message message = new Message(text, tag);
+        messageRepo.save(message);
+
+        //Get all messages
+        Iterable<Message> messages = messageRepo.findAll();
+        model.put("message", messages);
+
+        return "redirect:/";
+    }
+
+    @PostMapping("filter")
+    public String filterMessages(
+            @RequestParam String filter,
+            Map<String, Object> model
+    ){
+        Iterable<Message> messages;
+        if(filter!=null && !filter.isEmpty()){
+            messages = messageRepo.findByTag(filter);
+        }
+        else {
+            messages = messageRepo.findAll();
+        }
+        model.put("messages", messages);
+        return "home";
+    }
+
 }
