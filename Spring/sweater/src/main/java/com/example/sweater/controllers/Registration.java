26d4463caf0720d819403456671846f0f863c164
deviceai <device.ai@gmail.com>
2020-05-11 18:49:06 +0300

    sweater added user registration confirmation by email

--- Spring/sweater/src/main/java/com/example/sweater/controllers/Registration.java
+++ Spring/sweater/src/main/java/com/example/sweater/controllers/Registration.java
@@ -3,10 +3,12 @@ package com.example.sweater.controllers;
 import com.example.sweater.domain.Role;
 import com.example.sweater.domain.User;
 import com.example.sweater.repo.UserRepo;
+import com.example.sweater.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.Collections;
@@ -15,7 +17,7 @@ import java.util.Map;
 @Controller
 public class Registration {
     @Autowired
-    private UserRepo userRepo;
+    private UserService userService;
 
     @GetMapping("/registration")
     public String registration (){
@@ -24,15 +26,26 @@ public class Registration {
 
     @PostMapping("/registration")
     public String addUser(User user, Map<String, Object> model){
-        User userFromDb = userRepo.findByUsername(user.getUsername());
-        if (userFromDb != null){
+
+        if (!userService.addUser(user)){
             model.put("message", "User exists!");
             return "registration";
         }
 
-        user.setActive(true);
-        user.setRoles(Collections.singleton(Role.USER));
-        userRepo.save(user);
         return "redirect:/login";
     }
+
+    @GetMapping("/activate/{code}")
+    public String active(Model model, @PathVariable String code){
+
+        boolean isActivated = userService.activateUser(code);
+
+        if (isActivated){
+            model.addAttribute("message", "User successfully activated!");
+        } else {
+            model.addAttribute("message", "Activation code is not found!");
+        }
+        return "login";
+    }
+
 }
