a7412144f5a5581febade76701cf51a6015c7990
deviceai <device.ai@gmail.com>
2020-05-13 17:15:04 +0300

    sarafan return messages as json

--- Spring/sarafan/src/main/java/deviceai/sarafan/controller/MessageController.java
+++ Spring/sarafan/src/main/java/deviceai/sarafan/controller/MessageController.java
@@ -4,12 +4,22 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 @RestController
 @RequestMapping("/message")
 public class MessageController {
 
+    //Creating some list of messages
+    public List<Map<String,String>> messages = new ArrayList<Map<String, String>>(){{
+        add(new HashMap<String, String>() {{ put("id", "1"); put("text", "first message here");}});
+    }};
+
     @GetMapping
-    public String list() {
-        return "index";
+    public List<Map<String, String>> list() {
+        return messages;
     }
 }
