f7b6a9590b9f27b8cf25bebb9c20a188293069ed
deviceai <device.ai@gmail.com>
2020-05-14 13:36:51 +0300

    sarafan added vue.js - view data from server with vue

--- Spring/sarafan/src/main/resources/static/js/main.js
+++ Spring/sarafan/src/main/resources/static/js/main.js
@@ -1,3 +1,5 @@
+var messageApi = Vue.resource('/message/{id}');
+
 Vue.component('message-row',{
     props: ['message'],
     template: '<div><i>({{message.id}})</i> {{message.text}}</div>'
@@ -5,7 +7,14 @@ Vue.component('message-row',{
 
 Vue.component('messages-list', {
     props: ['messages'],
-    template: '<div><message-row v-for="message in messages" :message="message" :key="message.id"/></div>'
+    template: '<div><message-row v-for="message in messages" :message="message" :key="message.id"/></div>',
+    created: function () {
+        messageApi.get().then(result =>
+            result.json().then(data =>
+                data.forEach(message => this.messages.push(message))
+            )
+        )
+    }
 });
 
 var app = new Vue({
@@ -13,9 +22,7 @@ var app = new Vue({
     template: '<messages-list :messages="messages"/>',
     data: {
         messages: [
-            {id: '1', text: 'one'},
-            {id: '2', text: 'two'},
-            {id: '3', text: 'three'}
+
         ]
     }
 });
