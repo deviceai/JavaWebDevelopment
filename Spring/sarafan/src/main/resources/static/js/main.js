eedf6aa34cf90f323ab0240f2510134fadc998dd
deviceai <device.ai@gmail.com>
2020-05-14 13:15:16 +0300

    sarafan added vue.js - hard coded list view

--- Spring/sarafan/src/main/resources/static/js/main.js
+++ Spring/sarafan/src/main/resources/static/js/main.js
@@ -1,6 +1,21 @@
+Vue.component('message-row',{
+    props: ['message'],
+    template: '<div><i>({{message.id}})</i> {{message.text}}</div>'
+})
+
+Vue.component('messages-list', {
+    props: ['messages'],
+    template: '<div><message-row v-for="message in messages" :message="message" :key="message.id"/></div>'
+});
+
 var app = new Vue({
     el: '#app',
+    template: '<messages-list :messages="messages"/>',
     data: {
-        message: 'Hello Vue User!'
+        messages: [
+            {id: '1', text: 'one'},
+            {id: '2', text: 'two'},
+            {id: '3', text: 'three'}
+        ]
     }
-})
+});
